53b49bdd8a520c246eed67319fc6f62bbb1f7247
KeisukeToyota <hm.pudding0715@gmail.com>
2018-08-10 01:50:27 +0900

    commit

--- elm/Main.elm
+++ elm/Main.elm
@@ -1,10 +1,77 @@
 module Main exposing (..)
 
-import Html exposing (div, text, Html)
-import Html.Attributes exposing (class,id)
+import Html exposing (Html, button, div, text, program)
+import Html.Events exposing (onClick)
 
-hello =
-  div [] [text "Hello world"]
 
-main = div [class "p-main"]
-           [hello]
+-- モデル
+
+
+type alias Model =
+    Bool
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( False, Cmd.none )
+
+
+
+-- メッセージ
+
+
+type Msg
+    = Expand
+    | Collapse
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    if model then
+        div []
+            [ button [ onClick Collapse ] [ text "Collapse" ]
+            , text "Widget"
+            ]
+    else
+        div []
+            [ button [ onClick Expand ] [ text "Expand" ] ]
+
+
+
+-- 更新
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Expand ->
+            ( True, Cmd.none )
+
+        Collapse ->
+            ( False, Cmd.none )
+
+
+
+-- サブスクリプション(購読)
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- MAIN
+
+
+main =
+    program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
